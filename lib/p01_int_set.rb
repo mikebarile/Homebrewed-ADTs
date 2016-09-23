78bb1b53bc676a6f43dc66526478da0776b2b9cd
App Academy Student <student@appacademy.io>
2016-09-23 10:50:30 -0700

    create MaxIntSet IntSet and ResizingIntSet classes

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,19 +1,26 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max){false}
   end
 
   def insert(num)
+    @store[num] = true if is_valid?(num)
   end
 
   def remove(num)
+    @store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    @store[num] if is_valid?(num)
   end
 
   private
 
   def is_valid?(num)
+    raise 'Out of bounds' if num < 0 || num >= @max
+    true
   end
 
   def validate!(num)
@@ -27,18 +34,21 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +65,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      @count += 1
+      resize! if @count >= num_buckets
+      self[num] << num
+    end
   end
 
   def remove(num)
+    if include?(num)
+      @count -= 1
+      self[num].delete(num)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +94,11 @@ class ResizingIntSet
   end
 
   def resize!
+    two_bucks = num_buckets * 2
+    new_store = Array.new(two_bucks) { Array.new }
+    @store.flatten.each do |int|
+      new_store[int % two_bucks] << int
+    end
+    @store = new_store
   end
 end
